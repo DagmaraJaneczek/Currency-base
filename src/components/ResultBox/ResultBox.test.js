25841f6b61b25258e41f0c0e2a4d1b6d8f95cd09
DagmaraJaneczek <janeczek.dagmara@gmail.com>
2023-01-17 17:25:58 +0100

    Add others scenarios to teste of ResultBox component

--- src/components/ResultBox/ResultBox.test.js
+++ src/components/ResultBox/ResultBox.test.js
@@ -5,44 +5,49 @@ import '@testing-library/jest-dom/extend-expect';
 
 describe('Component ResultBox', () => {
     it('should render without crashing', () => {
-        render(<ResultBox from="PLN" to="USD" amount={100} />)
+
+        const testCases = [
+            {from: 'PLN', to: 'USD', amount: '12'},
+            {from: 'PLN', to: 'USD', amount: '20'},
+            {from: 'PLN', to: 'USD', amount: '2'},
+            {from: 'PLN', to: 'USD', amount: '100'},
+
+            {from: 'PLN', to: 'PLN', amount: '30'},
+
+            {from: 'USD', to: 'PLN', amount: '3.43'},
+            {from: 'USD', to: 'PLN', amount: '5.72'},
+            {from: 'USD', to: 'PLN', amount: '0.57'},
+            {from: 'USD', to: 'PLN', amount: '28.57'}
+        ];
+
+        for(const testObj of testCases) {
+            render(<ResultBox from={testObj.from} to={testObj.to} amount={testObj.amount} />);
+        };
     });
 
     it('should render proper info about conversion when PLN -> USD', () => {
-
-        const testCases = [
-            {from: 'PLN', to: 'USD', amount: '10', output: 'PLN 12.00 =  $3.43'},
-            {from: 'PLN', to: 'USD', amount: '20', output: 'PLN 20.00 =  $5.71'},
-            {from: 'PLN', to: 'USD', amount: '2', output: 'PLN 2.00 =  $0.57'},
-            {from: 'PLN', to: 'USD', amount: '100', output: 'PLN 100.00 =  $28.57'},
-        ];
-
-        for(const testObj of testCases) {
-            render(<ResultBox from={testObj.from} to={testObj.to} amount={testObj.amount} />);
-            const output = screen.getByTestId('output');
-            expect(output).toHaveTextContent(testObj.output);
-
-            cleanup();
-        }
-
+        render(<ResultBox from='PLN' to='USD' amount={100} />)
+        const output = screen.getByTestId('output')
+        expect(output).toHaveTextContent('PLN 100.00 = $28.57');
     });
 
     it('should render proper info about conversion when USD -> PLN', () => {
-
-        const testCases = [
-            {from: 'USD', to: 'PLN', amount: '3.43', output: '$3.43 = PLN 12.00'},
-            {from: 'USD', to: 'PLN', amount: '5.71', output: '$5.71 = PLN 20.00'},
-            {from: 'USD', to: 'PLN', amount: '0.57', output: '$0.57 = PLN 2.00'},
-            {from: 'USD', to: 'PLN', amount: '28.57', output: '$28.57 = PLN 100.00'},
-            {from: 'USD', to: 'USD', amount: '28.57', output: '$28.57 = $28.57'}
-        ];
-
-        for(const testObj of testCases) {
-            render(<ResultBox from={testObj.from} to={testObj.to} amount={testObj.amount} />);
-            const output = screen.getByTestId('output');
-            expect(output).toHaveTextContent(testObj.output);
-
-            cleanup();
-        }
+        render(<ResultBox from='USD' to='PLN' amount={5.72} />)
+        const output = screen.getByTestId('output')
+        expect(output).toHaveTextContent('$5.72 = PLN 20.02');
     });
+
+    it('should render proper info when same curreny is selected', () => {
+        render(<ResultBox from ='PLN' to='PLN' amount={30}/>)
+        const output = screen.getByTestId('output')
+        expect(output).toHaveTextContent('PLN 30.00 = PLN 30.00');
+    });
+
+    it('should render "Wrong value" when value is smaller than zero', () => {
+        render(<ResultBox from ='PLN' to='USD' amount={-100} />)
+        const output = screen.getByTestId('output');
+        expect(output).toHaveTextContent('Wrong value');
+    });
+
+    cleanup();
 });
